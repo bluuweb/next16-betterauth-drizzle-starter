886eceb71d88c2ac53ef48df42f476a0058a0f87
bluuweb <contacto@bluuweb.cl>
2026-03-13 18:57:12 -0300

    feat: actualizar DashboardPage para mostrar información del usuario en lugar de la sesión

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -5,12 +5,12 @@ import UserInfoProvider from "@/components/user-info-provider";
 import { useSession } from "@/lib/providers/session-provider";
 
 const DashboardPage = () => {
-  const session = useSession();
+  const { user } = useSession();
 
   return (
     <div>
       <h1>Dashboard</h1>
-      <pre>{JSON.stringify(session, null, 2)}</pre>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
       <SignOut />
       <UserInfoProvider />
     </div>
